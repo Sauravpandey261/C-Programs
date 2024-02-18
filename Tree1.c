5045cca7529937eea1ad01414cacdb7402104f1d
Saurav Pandey <133730941+Sauravpandey261@users.noreply.github.com>
2024-02-19 01:11:40 +0530

    Add files via upload

---
+++ Tree1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+struct node{
+	int data;
+	struct node *left, *right;
+};
+struct node *create(){
+	int data;
+	newnode = (struct node *)malloc(sizeof(struct node));
+	printf("Press 0 to Exit:")
+}
