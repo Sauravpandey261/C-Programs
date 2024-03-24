23ec87cfffc0b772e4072961d646178c0be32a3c
Saurav Pandey <133730941+Sauravpandey261@users.noreply.github.com>
2024-03-25 01:19:01 +0530

    Add files via upload

---
+++ Enqueue&Dequeue_by_linked_list.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node *next;
+};
+struct node *front=NULL;
+struct node *rear=NULL;
+
+void enqueue(int x){
+	struct node *newnode;
+	newnode=(struct node *)malloc(sizeof(struct node));
+	newnode->data=x;
+	newnode->next=NULL;
+	if(front==NULL && rear==NULL){
+		front=rear=newnode;
+	}
+	else{
+		rear->next=newnode;
+		rear=newnode;
+	}
+}
+void dequeue(){
+	struct node *temp;
+	temp=front;
+	if(front==NULL&&rear==NULL){
+		printf("underflow");
+	}
+	else{
+		front=front->next;
+		free(temp);
+	}
+}
+void display(){
+	struct node *temp;
+	temp=front;
+	while(temp!=NULL){
+		printf("\n%d",temp->data);
+		temp=temp->next;
+	}
+}
+void peak(){
+	printf("\nfront element is %d",front->data);
+}
+int main(){
+	enqueue(10);
+	enqueue(20);
+	enqueue(30);
+	enqueue(40);
+	dequeue();
+	display();
+	peak();
+	
+}
