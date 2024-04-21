b8b3e3b9feff47e5d61201ecb87da742946928b4
Saurav Pandey <133730941+Sauravpandey261@users.noreply.github.com>
2024-04-21 23:31:26 +0530

    Add files via upload

---
+++ pointer swaping.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int swap(int a,int b);
+int swap(int a,int b){
+	int swap;
+	
+}
+int main(){
+	int a=5,b=6,c;
+	c=a;
+	a=b;
+	b=c;
+	printf("after swaping a is %d\n",a);
+	printf("after swaping b is %d\n",b);
+		
+}
