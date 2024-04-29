a7f48be1b273b66f38a676b6cf4ae6cd545389fc
Saurav Pandey <133730941+Sauravpandey261@users.noreply.github.com>
2024-04-29 11:32:21 +0530

    Add files via upload

---
+++ prime number by recursion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int prime(int n);
+int main(){
+	int num;
+	printf("enter the number :");
+	scanf("%d",&num);
+	int prime(int num);
+}
+int prime(int n){
+		int num,i,count=0;
+	for(i=1;i<=num;i++){
+	if(num%i==0){
+		count++;
+	}
+	return count;
+    int prime(int num );
+	if(count==2){
+		printf("prime no.");
+	}
+}
+}
+
